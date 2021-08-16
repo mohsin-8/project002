c317174d1f235d0f4de95a2048ee2a19fae3a0d8
mohsin-8 <asif_khan1970@hotmail.com>
2021-08-16 20:26:07 +0500

    practice useState

--- src/Room.js
+++ src/Room.js
@@ -4,15 +4,29 @@ import './Room.css';
 function Room() {
     let [isLit, setLit] = useState(true);
     let [isAge, setAge] = useState(0);
+
+    function lightColor() {
+        console.log("Lignt is Change Successfully");
+        setLit(!isLit);
+    }
+
+    function IncreaseAge() {
+        console.log('change Age');
+        setAge(++isAge);
+    }
+    function DecreaseAge() {
+        console.log('change Age');
+        setAge(--isAge);
+    }
     return (
         <div className={`room ${isLit ? "lit" : "dark"}`}>
-            <h2>This Room is {isLit ? 'light' : 'dark'}</h2>
-            <button onClick={() => setLit(!isLit)}>Light Button</button>
-            <button onClick={() => setLit(!isLit)}>Dark Button</button>
+            <h3>This Room is: {isLit ? "light" : "dark"}</h3>
+            <button onClick={lightColor}>Light theme</button>
+            <button onClick={lightColor}>Dark theme</button>
 
-            <h2>Age: {isAge} </h2>
-            <button onClick={() => setAge(++isAge)}>Increase Age</button>
-            <button onClick={() => setAge(--isAge)}>Decrease Age</button>
+            <h3>Update Age: {isAge}</h3>
+            <button onClick={IncreaseAge}>Increase Age</button>
+            <button onClick={DecreaseAge}>Decrease Age</button>
         </div>
     )
 }
