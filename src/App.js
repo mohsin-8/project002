9d700c2cc97edcb7827a2487e832c5d77ef264e2
mohsin-8 <asif_khan1970@hotmail.com>
2021-08-17 15:38:27 +0500

    Learn useState, useContext and useReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import './App.css';
 // import Hello from './Hello';
-import Room from './Room';
+import Parent from './Parent';
+import ValueContext from './ValueContext';
 
-function App(props) {
+function App() {
+  // let [number, setNumber] = useState(0);
+  // function updateNumber() {
+  //     console.log('update number successfully');
+  //     setNumber(++number)
+  //   }
+  let value = useState(20);
   return (
-    <div className="App">
-      {/* Hello World from {props.name} age is = {props.age}
+    <ValueContext.Provider value={value}>
+      <div className="App">
+        {/* Hello World from {props.name} age is = {props.age}
       <Hello firstname="Zia" /> */}
-      <Room />
-    </div>
+        <Parent />
+      </div>
+    </ValueContext.Provider>
   );
 }
 
---
+++ src/ValueContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ValueContext = React.createContext(19, () => { });
+
+export default ValueContext;
